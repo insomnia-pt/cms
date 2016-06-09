ac976a3c3cc8ab0330f85731f109622360dc896d
Miguel Pereira <miguel@insomnia.pt>
2016-06-09 04:14:15 +0100

    Bug fix's;

--- src/views/layouts/default.blade.php
+++ src/views/layouts/default.blade.php
@@ -233,7 +233,7 @@
 				    $(this).attr('data-html', 'true');
 				    $(this).attr('data-trigger', 'hover');
 				    $(this).attr('data-placement', 'top');
-				    $(this).attr('data-content', '<img width="150" src="{{ URL::to(Config::get('cms::config.dir')) }}/'+$(this).text().trim()+'" alt="" />');
+				    $(this).attr('data-content', '<img width="150" src="{{ URL::to(Config::get('cms::config.elfinder_dir')) }}/'+$(this).text().trim()+'" alt="" />');
 				    $(this).attr('data-original-title', 'Preview');
 				});
 				$('.popover').remove();
